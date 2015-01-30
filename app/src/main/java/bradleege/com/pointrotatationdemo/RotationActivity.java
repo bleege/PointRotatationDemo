3d3016f1bf109f2f0a0df6aabaa64f6b2c45b3e7
Brad Leege <bleege@gmail.com>
2015-01-29 21:56:42 -0600

    Implementing example code

--- app/src/main/java/bradleege/com/pointrotatationdemo/RotationActivity.java
+++ app/src/main/java/bradleege/com/pointrotatationdemo/RotationActivity.java
@@ -1,19 +1,47 @@
+// Based on http://stackoverflow.com/questions/7795028/get-new-position-of-coordinate-after-rotation-with-matrix
+
 package bradleege.com.pointrotatationdemo;
 
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+
+public class RotationActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
 
 
-public class RotationActivity extends ActionBarActivity {
+    private MyDrawing myDrawing;
+    private SeekBar mSeekbar;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rotation);
-    }
 
+        Rect rect = new Rect(150,150,440,630);
+
+        int x = (int) (rect.left + Math.random() * rect.width());
+        int y = (int) (rect.top + Math.random() * rect.height());
+        Point coordinate = new Point(x, y);
+
+
+        // To draw the rect we create a CustomView
+        myDrawing = new MyDrawing(this, rect, coordinate);
+
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.container);
+        rl.addView(myDrawing);
+
+
+        mSeekbar = (SeekBar)findViewById(R.id.seekBar1);
+        mSeekbar.setMax(360);
+        mSeekbar.setOnSeekBarChangeListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +64,22 @@ public class RotationActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+
+        Point newCoordinates = myDrawing.rotate(progress);
+
+
+        // Now -> our float[] pts contains the new x,y coordinates
+        Log.d("test", "Before Rotate myPoint(" + newCoordinates.x + "," + newCoordinates.y + ")");
+        myDrawing.invalidate();
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
